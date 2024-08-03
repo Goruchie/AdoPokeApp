45596fcfc12687ee25ed8bd743ad04af5e0bd124
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2024-08-03 20:22:56 -0300

    Add an icon and a title for the Winform.

--- AdoPokeApp/Form1.Designer.cs
+++ AdoPokeApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AdoPokeApp
 {
-    partial class Form1
+    partial class AdoPokeApp
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdoPokeApp));
+            this.SuspendLayout();
+            // 
+            // AdoPokeApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdoPokeApp";
+            this.Text = "AdoPokeApp";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
